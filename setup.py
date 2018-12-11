ad351ee35b090f3719a6dd9547f1fa671c618e6b
Mark Coombes <mark@markcoombes.ca>
2018-12-11 10:27:19 -0500

    v0.2.0 (#1)
    * Add request timeout and catch Timeout exception
    
    * Update README.md
    
    * Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.md').read()
 
 setuptools.setup(
     name="pykuna",
-    version="0.1.0",
+    version="0.2.0",
     author="Mark Coombes",
     author_email="mark@markcoombes.ca",
     description="Python3 library for interacting with the Kuna camera mobile API",
